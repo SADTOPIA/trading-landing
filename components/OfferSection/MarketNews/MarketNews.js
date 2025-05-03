b252b41000513e5151889f2b899adeb93639a9e8
SADTOPIA <116076264+SADTOPIA@users.noreply.github.com>
2025-05-03 14:24:56 +0300

    mobile css

--- components/OfferSection/MarketNews/MarketNews.js
+++ components/OfferSection/MarketNews/MarketNews.js
@@ -3,7 +3,10 @@ import styles from "./MarketNews.module.css"
 export default function MarketNews() {
   return (
     <aside className={styles["market-news"]}>
-      <h3>MARKET NEWS</h3>
+      <h3>
+        <p>MARKET</p>
+        <p>NEWS</p>
+      </h3>
       <ul>
         <li>Global Stocks Edge Higher Ahead of US CPI Data</li>
         <li>Euro Falls as ECB Signals Pause in Rate Hikes</li>
